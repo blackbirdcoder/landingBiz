5d3847d1852910112c832586447243ca5a88cd83
Eugene-TechnoNinja <technoninja@ukr.net>
2018-05-22 09:41:44 +0300

    start project

---
+++ gulpfile.js
@@ -0,0 +1,119 @@
+﻿//  gulp v.0.3.1 beta
+//  задачи для проекта "landing-business.hw", который выполняю в рамках курса ITVDN
+var gulp         = require('gulp'),
+    sass         = require('gulp-sass'),
+    browserSync  = require('browser-sync'),
+    concat       = require('gulp-concat'),
+    uglify       = require('gulp-uglifyjs'),
+    cssnano      = require('gulp-cssnano'),
+    rename       = require('gulp-rename'),
+    del          = require('del'),
+    imagemin     = require('gulp-imagemin'),
+    pngquant     = require('imagemin-pngquant'),
+    cache        = require('gulp-cache'),
+    autoprefixer = require('gulp-autoprefixer'),
+    sourcemaps   = require('gulp-sourcemaps'),
+    pug          = require('gulp-pug'),
+    spritesmith  = require('gulp.spritesmith');
+    
+    
+	
+gulp.task('sass', function() {
+	return gulp.src(['app/sass/**/*.scss', 'app/sass/**/*.sass' ])
+        .pipe(sourcemaps.init())
+		.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+        .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'],
+                {cascade: true}))
+        .pipe(sourcemaps.write('.'))        
+		.pipe(gulp.dest('app/css'))
+		.pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('scripts-min', function(){
+	return gulp.src('app/libs/jquery/dist/jquery.js')
+		.pipe(concat('libs.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('app/js'));
+});
+
+gulp.task('css-min', function(){
+	return gulp.src([
+            //Изменения
+	'app/css/base-style.css',
+        'app/css/final-style.css'
+	])
+		.pipe(cssnano())
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest('app/css'));
+});
+
+gulp.task('browser-sync', function(){
+	browserSync({
+		server: { 
+			baseDir: 'app'
+		}, notify: false
+	});
+});
+
+//Делаю задачу PUG
+gulp.task('pug-run', function buildHTML(){
+    return gulp.src('app/pug/**/*.jade')
+            .pipe(pug({
+                pretty: true
+            }))
+            .pipe(gulp.dest('app'));
+});
+
+gulp.task('clear', function(){
+    return del.sync('dist');
+});
+
+gulp.task('clear-cache', function(){
+    return cache.clearAll();
+});
+
+//Делаю задачу для работы со спрайтами
+gulp.task('sprite', function () {
+  var spriteData = gulp.src('app/img/res_ico/*.png').pipe(spritesmith({
+    imgName: 'sprite.png',
+    imgPath: '../img/sprite.png',
+    cssName: '_sprite.scss'
+  }));
+  return spriteData.img.pipe(gulp.dest('app/img')),
+         spriteData.css.pipe(gulp.dest('app/sass/'));        
+});
+
+//Провёл изменения добавил исключения
+gulp.task('img', function(){
+    return gulp.src(['!app/img/res_ico/**/*', '!app/img/res_ico', 'app/img/**/*/'])
+            .pipe(cache(imagemin({
+                interlaced: true,
+                progressive: true,
+                svgoPlugins: [{removeViewBox: false}],
+                une: [pngquant()]
+            })))
+            .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('watch', ['sprite', 'browser-sync', 'pug-run', 'sass', 'css-min', 'scripts-min'],function(){
+    //наблюдаю за PUG и перезагружаю браузер 
+    gulp.watch('app/pug/**/*.jade', ['pug-run'], browserSync.reload);
+	gulp.watch(['app/sass/**/*.scss', 'app/sass/**/*.sass' ], ['sass']);
+	gulp.watch('app/*.html', browserSync.reload);
+	gulp.watch('app/js/**/*.js', browserSync.reload);
+});
+
+gulp.task('bulid', ['clear', 'clear-cache', 'img', 'sass', 'scripts-min'], function(){
+    
+    var buildCss = gulp.src('app/css/*')
+            .pipe(gulp.dest('dist/css'));
+    
+    var buildFonts = gulp.src('app/fonts/**/*')
+            .pipe(gulp.dest('dist/fonts'));
+    
+    var buildJs = gulp.src('app/js/**/*')
+            .pipe(gulp.dest('dist/js'));
+    
+    var buildHtml = gulp.src('app/*.html')
+            .pipe(gulp.dest('dist'));
+});
