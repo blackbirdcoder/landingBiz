c52c1865987a84dcddd84fa89f64a9dc6e4f5442
Eugene-TechnoNinja <technoninja@ukr.net>
2018-06-29 20:48:00 +0300

    Implemented the form functional. I did the Js events for the form buttons at the click of a mouse on opening and closing the feedback form. I checked everything. The buttons open and close the form work.

---
+++ app/js/common.js
@@ -0,0 +1,13 @@
+/*Js скрипты для сайта landing-business.hw */
+
+/*Скрипт для открытия и закрытия формы обратной связи*/
+document.getElementById('btn-open').onclick = function() {
+    document.getElementById('form').style.display = 'block';
+};
+document.getElementById('btn-close').onclick = function() {
+    document.getElementById('form').style.display = 'none';
+};
+
+ 
+
+
