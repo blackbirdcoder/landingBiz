3fd06196ac6b44c42a4502e06b8a75c0db205bd0
Eugene-TechnoNinja <technoninja@ukr.net>
2019-05-02 22:26:06 +0300

    Script script to verify the email address is ready

--- app/php/checker-sender-form.php
+++ app/php/checker-sender-form.php
@@ -57,7 +57,6 @@ if(isset($_POST['send'])) {
         file_put_contents($fileClients, "$time_date $name $phone $company\n", FILE_APPEND);
         //данные на почту...
         mail('technoninja@localhost', 'Новый клиент', "$time_date $name $phone $company", "Content-Type: text/html; charset=UTF-8\r\n");
-        //added up
         //Данные для выводя при успешно поданной заявки
         $successfulTitle = '<p class="notice__title"><b>' . $name . '</b></p>';
         $successfulText  = '<p class="notice__txt">Your application for a free consultation is accepted. Our managers will contact you shortly.</p>
@@ -65,7 +64,6 @@ if(isset($_POST['send'])) {
     }      
  //Если ошибок нет тогда уведомляю что пользователь записан.
 if(!$error) {
-    //added up
     $noticeTitle = $successfulTitle;
     $noticeText = $successfulText;
 }
@@ -84,126 +82,21 @@ header('Refresh: 16; ../../index.html?#application');
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" href="../img/favicon/favicon.ico">
+    <link rel="stylesheet" href="css/notice.css">
     <title>Landing Business</title>
-    <style>
-        .page {
-            margin: 0;
-            padding: 4% 4% 4% 4%;
-            font-size: 16px;
-            font-family: Arial, sans-serif;
-            color: #454545;
-            background: #1287bf;            
-        }
-        @media (max-width: 1045px) {
-            .page {
-                font-size: 14px;
-            }
-        }
-        @media (max-width: 228px) {
-            .page {
-                font-size: 6vw;
-            }
-        }
-        .container {
-            max-width: 1024px;
-            margin: auto;
-            background: #ffffff;
-        }
-        .header {
-            padding: 4% 3% 3% 3%;
-        }
-        .logo { 
-            text-align: center;
-        }
-        .logo__pic {
-            width: 120px;
-            height: auto;
-        }
-        @media (max-width: 228px) {
-            .logo__pic {
-                width: 60%;
-            }
-        }
-        .logo__heading {
-            margin: 0;
-            letter-spacing: 1px;
-            font-size: 2.7em;
-            
-        }
-        .Logo__txt {
-            margin: 0;
-            letter-spacing: 1px;
-            font-size: 1.2em;
-        }
-        .notice {
-            text-align: center;
-            padding: 1% 3% 1% 3%;
-        }
-        .notice__title {
-            margin: 0;
-            padding: 0 0 1% 0;
-            font-size: 1.5em;
-        }
-        .notice__title_warning {
-            color: red;
-        }
-        .notice__txt {
-            margin: 0;
-            font-size: 1.3em;
-            line-height: 150%;
-        }
-        .notice__txt_warning {
-            color: red;
-        }
-        .box-btn {
-            padding: 2% 0 4% 0;
-            text-align: center;
-        }
-        @media (max-width: 596px) {
-            .box-btn {
-                padding: 6% 0 6% 0;
-            }
-        }
-        .btn {
-            display: inline-block;
-            padding: 0 40px 0 40px; 
-            height: 47px;
-            line-height: 47px;
-            border-radius: 5px;
-            background: #e62f31;
-            text-decoration: none;
-            text-transform: uppercase;
-            font-size: 1.1em;
-            font-weight: normal; 
-            letter-spacing: 1px; 
-            color: #ffffff;
-            transition: 0.5s;
-        }
-        .btn:hover{
-            background: #4CAF50;
-            -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
-        }
-        @media (max-width: 228px) {
-            .btn{
-                padding: 0 20% 0 20%; 
-                height: 10%;
-                line-height: 4vh;
-            }
-        }
-        
-
+    <style> 
     </style>
   </head>
-<body class="page">
-    <div class="container"> 
+<body style="background:#1287bf;" class="page">
+    <div style="background:#ffffff;" class="container"> 
         <header class="header">
-            <div class="logo">
+            <div style="color:#454545;" class="logo">
                 <img src="pic/logo-biz-cap.svg" alt="Our company logo" class="logo__pic">
                 <h2 class="logo__heading">Business</h2>
                 <p class="Logo__txt">Tagline goes here</p>
             </div>
         </header>
-        <main class="notice">
+        <main style="color:#454545;" class="notice">
             <!-- <p class="notice__title"><b></b></p> -->
             <?php
             echo $noticeTitle; 
