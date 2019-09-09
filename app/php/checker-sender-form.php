5a7a727a95e5b01d0c4ced9f4bfcddffa02338ed
Eugene-TechnoNinja <technoninja@ukr.net>
2019-09-09 16:08:39 +0300

    Removed unnecessary

--- app/php/checker-sender-form.php
+++ app/php/checker-sender-form.php
@@ -1,27 +1,24 @@
 <?php
 //------------------------------------------------------------//
-//--Скрипт CHECKER-SENDER-FORM для сайта landing-business.hw--//
+//--CHECKER-SENDER-FORM  landing-business.hw--//
 //------------------------------------------------------------//
 
-//Переменные
+//var
 $name = '';
 $error = false;
 $directory = '../clients/';
 $fileClients = $directory.'people-clients.txt';
 $failureTitle = '<p class="notice__title notice__title_warning">Dear user</p>';
-//Определяю send не NULL (то-есть нажата была кнопка "Submit") тогда...
+
 if(isset($_POST['send'])) {
-    //Записываю данные из массива в переменные. 
-    //Делаю время в нужном формате, для удобства.
-    //Время будет в текстовом файле показывать когда человек отправил данные
+
     $time_date = date("Y-m-d H:i:s");
-    //Записываемые данные проходят через функцию которая убирает пробелы по краям текста. 
+
     $name = trim($_POST['name']);
     $phone = trim($_POST['phone']);
     $company = trim($_POST['company']);
 }  
-    //Обработка ошибок отправленных с формы.
-    //Идут проверки на недопустимые символы и на короткие имена.
+
     if($name == "" || preg_match("/[0-9\.+$^)(><\?\]\[\}\{\*\&\;\:\~\/\=\#\%\@\№\!\"]/u", $name)) {
             $error = true;
             $failureText = '<p class="notice__txt notice__txt_warning">The name is incorrect! Please send the application again!</p>';
@@ -47,32 +44,30 @@ if(isset($_POST['send'])) {
             $failureText = '<p class="notice__txt notice__txt_warning">Short company name! Please send the application again!</p>';
     }
     else{
-        //Проверяю есть каталог или нет и затем
-        //создаю каталог для хранения файла клиенты
+
         if(!file_exists($directory)) {
             mkdir($directory, 0777); 
         }    
-        //Данные которые не содержат ошибок, записываются в файл, а также отправляются на почту.
-        //запись в файл...
+       
         file_put_contents($fileClients, "$time_date $name $phone $company\n", FILE_APPEND);
-        //данные на почту...
+
         mail('technoninja@localhost', 'Новый клиент', "$time_date $name $phone $company", "Content-Type: text/html; charset=UTF-8\r\n");
-        //Данные для выводя при успешно поданной заявки
+
         $successfulTitle = '<p class="notice__title"><b>' . $name . '</b></p>';
         $successfulText  = '<p class="notice__txt">Your application for a free consultation is accepted. Our managers will contact you shortly.</p>
                     <p class="notice__txt">Thank you,&nbsp;<b>' . $name . '</b>, for choosing us!</p>';
     }      
- //Если ошибок нет тогда уведомляю что пользователь записан.
+
 if(!$error) {
     $noticeTitle = $successfulTitle;
     $noticeText = $successfulText;
 }
-//Если ошибка есть то указываю в чём проблема.
+
 if($error == true) {
     $noticeTitle = $failureTitle;
     $noticeText = $failureText;
 }
-//Через время возвращает назад к форме на сайт
+
 header('Refresh: 16; ../../index.html?#application');
 ?>
 <!DOCTYPE html>
